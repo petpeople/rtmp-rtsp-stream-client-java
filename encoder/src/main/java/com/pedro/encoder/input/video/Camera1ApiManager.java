1b57a5e0e78b1fd85ce920c49b09bdb6d9f4367f
Stanislav <statelo1992@gmail.com>
2022-08-22 18:24:13 +0900

    change camera number to 0

--- encoder/src/main/java/com/pedro/encoder/input/video/Camera1ApiManager.java
+++ encoder/src/main/java/com/pedro/encoder/input/video/Camera1ApiManager.java
@@ -140,7 +140,7 @@ public class Camera1ApiManager implements Camera.PreviewCallback, Camera.FaceDet
       // petpeople code start
       if( Camera.getNumberOfCameras() > 2 && cameraSelect == 0 ){
         Log.d("WAYO-TEST", String.valueOf(Camera.getNumberOfCameras()));
-        camera = Camera.open(1);
+        camera = Camera.open(0);
       } else {
         camera = Camera.open(cameraSelect);
       }
