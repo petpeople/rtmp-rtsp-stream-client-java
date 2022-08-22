b4f7dfee0279b29aa4edf073c5b150dae9004647
Stanislav <statelo1992@gmail.com>
2022-08-22 18:20:06 +0900

    change camera to 1

--- encoder/src/main/java/com/pedro/encoder/input/video/Camera1ApiManager.java
+++ encoder/src/main/java/com/pedro/encoder/input/video/Camera1ApiManager.java
@@ -140,7 +140,7 @@ public class Camera1ApiManager implements Camera.PreviewCallback, Camera.FaceDet
       // petpeople code start
       if( Camera.getNumberOfCameras() > 2 && cameraSelect == 0 ){
         Log.d("WAYO-TEST", String.valueOf(Camera.getNumberOfCameras()));
-        camera = Camera.open(3);
+        camera = Camera.open(1);
       } else {
         camera = Camera.open(cameraSelect);
       }
