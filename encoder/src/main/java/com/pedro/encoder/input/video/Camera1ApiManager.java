f8eb8be85b291d3a7893117598cdb7afa5e8c916
Stanislav <statelo1992@gmail.com>
2022-08-22 18:14:02 +0900

    add log

--- encoder/src/main/java/com/pedro/encoder/input/video/Camera1ApiManager.java
+++ encoder/src/main/java/com/pedro/encoder/input/video/Camera1ApiManager.java
@@ -139,7 +139,8 @@ public class Camera1ApiManager implements Camera.PreviewCallback, Camera.FaceDet
     try {
       // petpeople code start
       if( Camera.getNumberOfCameras() > 2 && cameraSelect == 0 ){
-        camera = Camera.open(Camera.getNumberOfCameras() - 1);
+        Log.d("WAYO-TEST", String.valueOf(Camera.getNumberOfCameras()));
+        camera = Camera.open(3);
       } else {
         camera = Camera.open(cameraSelect);
       }
